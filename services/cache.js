785bbac8ac11e3fa4d9ef56560b9ac453e4f5494
Song Cho <deeplearner507@gmail.com>
2019-06-13 00:02:04 +0900

    add cache clearing middleware

--- services/cache.js
+++ services/cache.js
@@ -4,21 +4,32 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+
+  return this;
+};
+
 mongoose.Query.prototype.exec = async function() {
-    console.log('about to run a query')
-    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
-      collection: this.mongooseCollection.name
-    }));
-    console.log(key)
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
+  console.log('about to run a query')
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  }));
 
   // See if we have a value for 'key' in redis
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // If we do, return that
   if (cacheValue) {
+    console.log("캐쉬 있네?")
     const doc = JSON.parse(cacheValue);
 
     return Array.isArray(doc)
@@ -28,7 +39,12 @@ mongoose.Query.prototype.exec = async function() {
 
   // Otherwise, issue the query and store the result in redis
   const result = await exec.apply(this, arguments);
-  client.set( key, JSON.stringify(result));
-
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
   return result
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
 };
