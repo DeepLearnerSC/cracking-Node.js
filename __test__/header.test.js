2672bacabefb848786aee5225fd906027d2d2c01
Song Cho <deeplearner507@gmail.com>
2019-06-26 21:09:28 +0900

    generating sessions and sig

--- __test__/header.test.js
+++ __test__/header.test.js
@@ -15,7 +15,7 @@ beforeEach(async () => {
 })
 
 afterEach(async () => {
-    await browser.close();
+    //await browser.close();
 })
 
 //console.log("sdf")
@@ -31,4 +31,28 @@ test('clicking login starts oauth flow', async () => {
 
     console.log(url)
     expect(url).toMatch(/accounts\.google\.com/);
+})
+
+test.only('When signed in, shows logout button', async () => {
+    const id ='5d03cd041c3fc71292a88ce3'; //from my MongoDB
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    }
+    const sessionString = Buffer.from(
+        JSON.stringify(sessionObject)
+    ).toString('base64');
+
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    await page.setCookie({name: "session", value: sessionString})
+    await page.setCookie({name: "session.sig", value: sig})
+    console.log(sessionString, sig);
+    await page.goto('localhost:3000');
 })
