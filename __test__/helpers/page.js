65a734e0613e64bfa226dca4cfec1b09bade8fdc
Song Cho <deeplearner507@gmail.com>
2019-06-28 00:19:10 +0900

    custom page implementation

---
+++ __test__/helpers/page.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const Session = require('../lib/session');
+const userFactory = require('../lib/user');
+
+class CustomPage {
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+
+    return new Proxy(customPage, {
+      get: function(target, property) {
+        return customPage[property] || browser[property] || page[property];
+      }
+    });
+  }
+
+  constructor(page) {
+    this.page = page;
+  }
+
+  async login() {
+    const user = await userFactory();
+    const { session, sig } = Session(user);
+
+    await this.page.setCookie({ name: 'session', value: session });
+    await this.page.setCookie({ name: 'session.sig', value: sig });
+    await this.page.goto('http://localhost:3000/blogs');
+    await this.page.waitFor('a[href="/auth/logout"]');
+  }
+}
+
+module.exports = CustomPage;
